73e8e50fad91ea2918d758ca82289bba79e1af9d
Trevor Buck <trevorbuck@Trevors-MacBook-Pro.local>
2021-05-16 12:05:45 -0400

    initialize repository

---
+++ lib/tbuck14_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tbuck14_palindrome/version"
+
+module Tbuck14Palindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tbuck14_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tbuck14Palindrome
+  VERSION = "0.1.0"
+end
