20af7f259d6e6a0c309c2a6e5091cc7327768ed9
Trevor Buck <trevorbuck@Trevors-MacBook-Pro.local>
2021-05-16 14:32:35 -0400

    added functionality to include integers

--- lib/tbuck14_palindrome.rb
+++ lib/tbuck14_palindrome.rb
@@ -2,17 +2,29 @@
 
 require_relative "tbuck14_palindrome/version"
 
-class String
+module Palindrome
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      scan(/[a-z]/i).join.downcase
+      to_s.scan(/[a-z0-9]/i).join.downcase
     end
+end
+
+class String
+  include Palindrome
+end
+
+class Integer
+  include Palindrome
 end
